b4adee4aebcdf52e5971ae597305a53a92492017
Tumpeti <petertumpek20@gmail.com>
2022-12-19 22:24:18 +0100

    hibás betű többször írható, nem von le érte életet

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from hangman_lists import logo, stages
 
 print(logo)
 chosen_word = random.choice(hangman_lists.word_list)
-print(chosen_word)
 word_lenght = len(chosen_word)
 lives = 6
 display = []
@@ -18,14 +17,17 @@ while not end_of_game:
     guess = input("Tippelj egy betűt: ").lower()
     if guess in display:
         print("Ezt a betűt már írtad!")
+    if guess in hangman_lists.already_guessed:
+        print("Ezt a betűt már írtad és nincs benne!")
     for position in range(word_lenght):
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
 
-    if guess not in chosen_word:
+    if guess not in chosen_word and guess not in hangman_lists.already_guessed:
         lives -= 1
         print(f'A(z) "{guess}" betű nincs benne a szóban, vesztettél egy életet.')
+        hangman_lists.already_guessed.append(guess)
 
     print(display)
     print(stages[lives])
